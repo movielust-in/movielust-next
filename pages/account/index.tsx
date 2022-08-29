50a6b1f498a67a92ba6056c3e60365f8c24299f0
Anurag <01anuragparmar@gmail.com>
2022-08-29 10:50:50 +0530

    ui fix account page

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -237,6 +237,12 @@ const Container = styled.div`
   @media (max-width: 724px) and (display-mode: browser) {
     padding: 0 30px;
   }
+
+  @media (max-width: 724px) and (display-mode: standalone),
+    (max-width: 870px) and (display-mode: standalone) {
+    padding-right: 30px;
+    padding-left: 30px;
+  }
 `;
 
 const slideInL = keyframes`
