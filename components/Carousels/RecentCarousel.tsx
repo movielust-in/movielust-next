8d4599ffe7e420bcfceb698898564c1d6bd5adef
Anurag Parmar <01anuragparmar@gmail.com>
2023-11-06 12:53:35 +0530

    chore: purged redux

--- components/Carousels/RecentCarousel.tsx
+++ components/Carousels/RecentCarousel.tsx
@@ -1,90 +1,90 @@
-import { useEffect, useRef } from 'react';
-import { SwiperSlide } from 'swiper/react';
-import Link from 'next/link';
+// import { useEffect, useRef } from 'react';
+// import { SwiperSlide } from 'swiper/react';
+// import Link from 'next/link';
 
-import { useDispatch, useSelector } from '../../redux';
-import { fetchWatched } from '../../helpers/user';
-import WrapH from '../CarouselSlices/Wrap_Horizantal';
-import { setRecents } from '../../redux/reducers/recent.reducer';
-import { detailLink } from '../../utils';
-import { detailLinkWithEpisode } from '../../utils/dashedTitle';
-import styles from '../../styles/recent_carousel.module.scss';
+// import { useDispatch, useSelector } from '../../redux';
+// import { fetchWatched } from '../../helpers/user';
+// import WrapH from '../CarouselSlices/Wrap_Horizantal';
+// import { setRecents } from '../../redux/reducers/recent.reducer';
+// import { detailLink } from '../../utils';
+// import { detailLinkWithEpisode } from '../../utils/dashedTitle';
+// import styles from '../../styles/recent_carousel.module.scss';
 
-import Carousel from './Carousel';
+// import Carousel from './Carousel';
 
-const breakPoints = {
-  1024: {
-    slidesPerView: 4,
-  },
-  464: {
-    slidesPerView: 4,
-  },
-  0: {
-    slidesPerView: 3,
-  },
-};
+// const breakPoints = {
+//   1024: {
+//     slidesPerView: 4,
+//   },
+//   464: {
+//     slidesPerView: 4,
+//   },
+//   0: {
+//     slidesPerView: 3,
+//   },
+// };
 
-function RecentCarousel() {
-  const recents = useSelector((state) => state.recents.content);
+// function RecentCarousel() {
+//   const recents = useSelector((state) => state.recents.content);
 
-  const isStale = useSelector((state) => state.recents.stale);
+//   const isStale = useSelector((state) => state.recents.stale);
 
-  const isAuthenticated = useSelector((state) => state.user.isLoggedIn);
+//   const isAuthenticated = useSelector((state) => state.user.isLoggedIn);
 
-  const dispatch = useDispatch();
+//   const dispatch = useDispatch();
 
-  const called = useRef(false);
+//   const called = useRef(false);
 
-  useEffect(() => {
-    if (isStale && !called.current && isAuthenticated) {
-      called.current = true;
-      fetchWatched().then((res) => {
-        dispatch(setRecents(res));
-      });
-    }
-  }, [dispatch, isStale, isAuthenticated]);
+//   useEffect(() => {
+//     if (isStale && !called.current && isAuthenticated) {
+//       called.current = true;
+//       fetchWatched().then((res) => {
+//         dispatch(setRecents(res));
+//       });
+//     }
+//   }, [dispatch, isStale, isAuthenticated]);
 
-  return recents.length > 0 ? (
-    <div className={styles.container}>
-      <h2>Recently viewed</h2>
-      <Carousel carosel_id="recent_carosel" breakPoints={breakPoints}>
-        {recents.map((content: any) => (
-          <SwiperSlide
-            key={
-              content.title ||
-              `${content.name} S${content.season_number} E${content.episode_number}`
-            }
-          >
-            <Link
-              prefetch={false}
-              href={
-                content.media_type === 'movie'
-                  ? detailLink(content.media_type, content.id, content.title)
-                  : detailLinkWithEpisode(
-                      content.media_type,
-                      content.id,
-                      content.name,
-                      content.season_number,
-                      content.episode_number
-                    )
-              }
-            >
+//   return recents.length > 0 ? (
+//     <div className={styles.container}>
+//       <h2>Recently viewed</h2>
+//       <Carousel carosel_id="recent_carosel" breakPoints={breakPoints}>
+//         {recents.map((content: any) => (
+//           <SwiperSlide
+//             key={
+//               content.title ||
+//               `${content.name} S${content.season_number} E${content.episode_number}`
+//             }
+//           >
+//             <Link
+//               prefetch={false}
+//               href={
+//                 content.media_type === 'movie'
+//                   ? detailLink(content.media_type, content.id, content.title)
+//                   : detailLinkWithEpisode(
+//                       content.media_type,
+//                       content.id,
+//                       content.name,
+//                       content.season_number,
+//                       content.episode_number
+//                     )
+//               }
+//             >
 
-              <WrapH
-                src={`https://image.tmdb.org/t/p/w300/${content.backdrop_path}`}
-                alt={content.title || content.name || ''}
-                title={
-                  content.title ||
-                  `${content.name} S${content.season_number} E${content.episode_number}`
-                }
-              />
+//               <WrapH
+//                 src={`https://image.tmdb.org/t/p/w300/${content.backdrop_path}`}
+//                 alt={content.title || content.name || ''}
+//                 title={
+//                   content.title ||
+//                   `${content.name} S${content.season_number} E${content.episode_number}`
+//                 }
+//               />
 
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Carousel>
-    </div>
-  ) : null;
-}
+//             </Link>
+//           </SwiperSlide>
+//         ))}
+//       </Carousel>
+//     </div>
+//   ) : null;
+// }
 
-export default RecentCarousel;
+// export default RecentCarousel;
--- components/ReduxProvider.tsx
+++ components/ReduxProvider.tsx
@@ -1,12 +1,12 @@
-'use client';
+// 'use client';
 
-import { ReactNode } from 'react';
-import { Provider } from 'react-redux';
+// import { ReactNode } from 'react';
+// import { Provider } from 'react-redux';
 
-import store from '../redux/store';
+// import store from '../redux/store';
 
-const ReduxProvider = ({ children }: { children: ReactNode }) => (
-  <Provider store={store}>{children}</Provider>
-);
+// const ReduxProvider = ({ children }: { children: ReactNode }) => (
+//   <Provider store={store}>{children}</Provider>
+// );
 
-export default ReduxProvider;
+// export default ReduxProvider;
