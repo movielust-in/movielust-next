3095fe2bd11b5bdc625d436a656fee0994cff649
Anurag Parmar <01anuragparmar@gmail.com>
2023-11-09 22:57:34 +0530

    middleware debugging

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
+/* eslint-disable no-console */
 import { NextRequest, NextResponse } from 'next/server';
 
 // eslint-disable-next-line consistent-return
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('next-auth.session-token')?.value;
+
+  console.log('token', token);
+  console.log('request', request);
 
   if (!token && request.nextUrl.pathname.startsWith('/api/user')) {
     return Response.json(
